c28f2f6ba12ab139b06a36e4e09113e0dc9a1a13
Rebecca Mollica <rebecca.mollica@live.it>
2021-10-19 16:33:08 +0200

    primo commit esercizi

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//let animal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda ");
+
+let animal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda ");
+
+if (animal == 1){
+
+    console.log("Chose the Dog")
+} else if (animal == 2){
+
+    console.log("Chose the Cat")
+} else if (animal == 3){
+
+    console.log("Chose the Goldfish")
+} else if (animal == 4){
+
+    console.log("Chose the Monkey")
+} else if (animal == 5){
+
+    console.log("Chose the Panda")
+} else {
+    
+    console.log("Animal not found")
+
+}
